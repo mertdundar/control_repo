e66c38ca31d4bdc385851cd7293ae5707019fbcb
Mert Dündar <mert.dundar@hotmail.com>
2022-04-08 17:59:50 +0300

    Update init.pp

--- site/minecraftmodule/manifests/init.pp
+++ site/minecraftmodule/manifests/init.pp
@@ -21,7 +21,7 @@ class minecraftmodule (
     version_build => '12',
     basedir       => '/usr/lib/jvm',
     }
-  file {"{$install_dir}/eula.txt":
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -34,7 +34,7 @@ class minecraftmodule (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
+    require => [File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
   
 }
