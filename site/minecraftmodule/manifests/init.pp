3744a6a5f76cd6742321036113154d38caf2e467
Mert Dündar <mert.dundar@hotmail.com>
2022-04-08 17:51:46 +0300

    Update init.pp

--- site/minecraftmodule/manifests/init.pp
+++ site/minecraftmodule/manifests/init.pp
@@ -34,7 +34,7 @@ class minecraftmodule (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java::adoptium'],File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
   
 }
