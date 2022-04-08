ef6fec730d84192209c23d48017f89bf5131088b
Mert Dündar <mert.dundar@hotmail.com>
2022-04-08 17:41:46 +0300

    Update init.pp

--- site/minecraftmodule/manifests/init.pp
+++ site/minecraftmodule/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraftmodule {
-  $url = 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar'
+class minecraftmodule (
+  $url = 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   $install_dir = '/opt/minecraft'
+  ) {
   file {$install_dir:
     ensure => directory,
   }
