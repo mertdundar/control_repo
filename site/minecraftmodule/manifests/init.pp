943b098a17d4376ec195ba06870ffb33704628e2
Mert Dündar <mert.dundar@hotmail.com>
2022-04-08 16:51:25 +0300

    Update init.pp

--- site/minecraftmodule/manifests/init.pp
+++ site/minecraftmodule/manifests/init.pp
@@ -6,8 +6,13 @@ class minecraftmodule {
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
-  package {'java-11-openjdk':
-    ensure => present,
+  #package {'java-11-openjdk':
+  #  ensure => present,
+  #}
+  java::download { 'jdk-18' :
+    ensure  => 'present',
+    java_se => 'jdk',
+    url     => 'https://download.java.net/openjdk/jdk18/ri/openjdk-18+36_linux-x64_bin.tar.gz",
   }
   file {'/opt/minecraft/eula.txt':
     ensure => file,
