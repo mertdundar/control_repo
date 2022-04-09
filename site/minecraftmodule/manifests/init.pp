cfe8ef8040a6064a362eb4ba0afe30c69c476489
Mert Dündar <mert.dundar@hotmail.com>
2022-04-09 16:02:54 +0300

    Update init.pp

--- site/minecraftmodule/manifests/init.pp
+++ site/minecraftmodule/manifests/init.pp
@@ -20,7 +20,7 @@ class minecraftmodule (
     version_patch => '1',
     version_build => '12',
     basedir       => "${install_dir}/jvm",
-    before => [File['/etc/environment'],Service['minecraft']],
+    before => [File["${install_dir}/jvm/jdk-17.0.1+12/bin"],File['/etc/environment'],Service['minecraft']],
     }
   file { "${install_dir}/jvm/jdk-17.0.1+12/bin":
     ensure => directory,
@@ -44,7 +44,7 @@ class minecraftmodule (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
+    require => [File["${install_dir}/jvm/jdk-17.0.1+12/bin"],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
   
 }
